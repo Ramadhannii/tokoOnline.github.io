73fab5f3782556a595c544ae652a302d6cf687ee
Ramadhannii <hajiadittt@gmail.com>
2024-01-27 15:21:53 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marketplace</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <nav class="nav">
+        <div class="min-content">
+            <div class="navbar">
+                <!-- <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div> -->
+                <div class="logo">
+                    <a href="">Bebas</a>
+                </div>
+                <div class="search">
+                    <input type="text" name="search" placeholder="Cari dibebas...">
+                    <button type="submit" name="btn-search"> Search</button>
+                </div>
+                <div class="info">
+                    <ul>
+                        <li><a href="">FAQ</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <section class="min-content">
+        <div class="container-content">
+            <h3>KATEGORI</h3>
+            <div class="content-kategori">
+                <div class="box-pakaian">
+                    <a href=""><img src="_img/sweater.jpg" alt=""></a>
+                    <a href="">Sweater</a>
+                </div>
+                <div class="box-pakaian">
+                    <a href=""><img src="_img/sepatu.jpg" alt=""></a>
+                    <a href="">Sepatu</a>
+                </div>
+                <div class="box-pakaian">
+                    <a href=""><img src="_img/tas.jpg" alt=""></a>
+                    <a href="">Tas</a>
+                </div>
+                <div class="box-pakaian">
+                    <a href=""><img src="_img/kaos.jpg" alt=""></a>
+                    <a href="">Kaos</a>
+                </div>
+                <div class="box-pakaian">
+                    <a href=""><img src="_img/hoodie.jpg" alt=""></a>
+                    <a href="">Hoodie</a>
+                </div>
+                <div class="box-pakaian">
+                    <a href=""><img src="_img/celana.jpg" alt=""></a>
+                    <a href="">Celana</a>
+                </div>
+                <div class="box-pakaian">
+                    <a href=""><img src="_img/kemeja.jpg" alt=""></a>
+                    <a href="">Kemeja</a>
+                </div>
+                <div class="box-pakaian">
+                    <a href=""><img src="_img/ttopi.jpg" alt=""></a>
+                    <a href="">Topi</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="min-content">
+        <div class="container-produk">
+            <div class="header-produk">
+                <h3>REKOMENDASI</h3>
+            </div>
+            <a href="">
+                <div class="shopping-cart">
+                    <div class="gambar-produk">
+                        <img src="_img/kaos.jpg" alt="">
+                    </div>
+                    <div class="deskripsi">
+                        <p>Kaos Dobuject motif garis</p>
+                        <br>
+                        <p>Rp8x.xxx</p>
+                    </div>
+                </div>
+            </a>
+
+            </div>
+    </section>
+
+    
+</body>
+</html>
